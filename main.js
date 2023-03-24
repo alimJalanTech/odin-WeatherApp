80487f2155afbd2dc3f56045bc4d60453ac1466b
Alim <mohammad.alim@jalantechnologies.com>
2023-03-24 12:15:06 +0530

    Commit: PR

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-(()=>{const e=document.querySelector(".search input");document.querySelector(".btn").addEventListener("click",(()=>{e.value&&async function(e){const t=await fetch("https://api.openweathermap.org/data/2.5/weather/?units=metric&q="+e+"&appid=aaebd4178fcf227a3349fb75ef211df7");var n=await t.json();document.querySelector(".city").innerHTML=n.name,document.querySelector(".temp").innerHTML=Math.round(n.main.temp)+"°C",document.querySelector(".humidity").innerHTML=n.main.humidity+"%",document.querySelector(".wind").innerHTML=n.wind.speed+"km/h"}(e.value)}))})();
+(() => {
+  const e = document.querySelector(".search input");
+  document.querySelector(".btn").addEventListener("click", () => {
+    e.value &&
+      (async function (e) {
+        const t = await fetch(
+          "https://api.openweathermap.org/data/2.5/weather/?units=metric&q=" +
+            e +
+            "&appid=aaebd4178fcf227a3349fb75ef211df7"
+        );
+        var n = await t.json();
+          (document.querySelector(".city").innerHTML = n.name),
+          (document.querySelector(".temp").innerHTML =
+            Math.round(n.main.temp) + "°C"),
+          (document.querySelector(".humidity").innerHTML =
+            n.main.humidity + "%"),
+          (document.querySelector(".wind").innerHTML = n.wind.speed + "km/h");
+      })(e.value);
+  });
+})();
