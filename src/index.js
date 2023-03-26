5091325c3f0f6d919a23e77815e65238ab3406ba
Alim <mohammad.alim@jalantechnologies.com>
2023-03-27 03:32:24 +0530

    Commit: Configure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-const API_KEY = "aaebd4178fcf227a3349fb75ef211df7";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather/?units=metric&q=";
+import {keys} from "./config.js";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".btn");
 
 async function checkWeather (city) {
-    const response = await fetch(API_URL + city+  `&appid=${API_KEY}`);
-    var data = await response.json();
+    const response = await fetch(`${keys.API_URL}${city}&appid=${keys.API_KEY}`);
+    const data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
 }
 
 searchBtn.addEventListener("click", () =>  {
